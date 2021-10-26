266d7cac9bfe38eb42d1feae0a816c0b6c761ba5
lshadler <lucas_shadler@intuit.com>
2021-10-26 17:17:34 -0400

    fix: testing without build requirement

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,6 @@
-const assert = require('assert');
-const strings = require('../compile_test/strings.en.js')
+import assert from 'assert';
+import * as strings from '../src/strings.en.js';
+
 describe('strings', function() {
     it('should print hello webapp', function() {
       assert.equal(strings.hello(), 'Hello Webapp');
